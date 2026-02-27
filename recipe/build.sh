829bc6acc3ec721582125027692136c2c959eeca
Divon Lan <divon@shablife.com>
2026-02-27 02:01:42 +0200

    update

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 
-make -C src install
+mkdir -p src/objdir.osx-arm/{zlib,bzlib,lzma,libdeflate_1.7,libdeflate_1.19,libdeflate_1.19/{x86,arm},bsc,htscodecs,igzip/{noarch}}
+make -j -C src install
